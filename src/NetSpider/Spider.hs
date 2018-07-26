a4adee064e977459b83adaf4efa9ef85156aa0ce
Toshio Ito <debug.ito@gmail.com>
2018-07-26 18:56:36 +0900

    Spider: add skeleton of connectWS and close.

--- src/NetSpider/Spider.hs
+++ src/NetSpider/Spider.hs
@@ -6,17 +6,31 @@
 -- 
 module NetSpider.Spider
        ( Spider,
+         connectWS,
+         close,
          addNeighbors,
          getLatestSnapshot
        ) where
 
 import Data.Vector (Vector)
+import Network.Greskell.WebSocket
+  ( Host, Port
+  )
 
 import NetSpider.Neighbors (Neighbors)
 import NetSpider.Snapshot (SnapshotElement)
 
 -- | An IO agent of the NetSpider database.
 data Spider = Spider
+
+-- | Connect to the WebSocket endpoint of Tinkerpop Gremlin Server
+-- that hosts the NetSpider database.
+connectWS :: Host -> Port -> IO Spider
+connectWS = undefined
+
+-- | Close and release the 'Spider' object.
+close :: Spider -> IO ()
+close = undefined
 
 -- | Add an observation of 'Neighbors' to the NetSpider database.
 addNeighbors :: Spider -> Neighbors n p -> IO ()
