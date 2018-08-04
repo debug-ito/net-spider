b61047b57c0037b8153c8bd4c37467959d40e440
Toshio Ito <debug.ito@gmail.com>
2018-08-04 14:48:20 +0900

    ServerTest: add showSubmitException for debug.

--- test/ServerTest.hs
+++ test/ServerTest.hs
@@ -1,11 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main,spec) where
 
-import Control.Exception.Safe (bracket)
+import Control.Exception.Safe (bracket, withException)
+import Control.Monad (mapM_)
+import Data.Aeson (Value(..))
+import qualified Data.HashMap.Strict as HM
 import Data.List (sort)
-import Data.Text (Text)
+import Data.Monoid ((<>))
+import Data.Text (Text, unpack)
+import qualified Data.Text.IO as TIO
 import Data.Vector (Vector)
 import qualified Data.Vector as V
+import qualified Network.Greskell.WebSocket as Gr
+import qualified Network.Greskell.WebSocket.Response as Res
+import System.IO (stderr)
 import Test.Hspec
 import Test.Hspec.NeedEnv (needEnvHostPort, EnvMode(Need))
 
@@ -52,8 +60,9 @@ spec_getLatestSnapshot = withServer $ describe "getLatestSnapshot" $ do
                           observedTime = fromEpochSecond 100,
                           neighborLinks = return link
                         }
-    addNeighbors spider nbs
-    got <- fmap (sort . V.toList) $ (getLatestSnapshot spider :: IO (Vector (SnapshotElement Text Text)))
+    flip withException showSubmitException $ addNeighbors spider nbs
+    got <- flip withException showSubmitException
+           $ fmap (sort . V.toList) $ (getLatestSnapshot spider :: IO (Vector (SnapshotElement Text Text)))
     let [Left got_n1, Left got_n2, Right got_link] = got
     nodeId got_n1 `shouldBe` "n1"
     isOnBoundary got_n1 `shouldBe` False
@@ -64,3 +73,21 @@ spec_getLatestSnapshot = withServer $ describe "getLatestSnapshot" $ do
     linkTimestamp got_link `shouldBe` fromEpochSecond 100
 
 -- TODO: how linkState relates to the property of SnapshotLink ?
+
+showSubmitException :: Gr.SubmitException -> IO ()
+showSubmitException (Gr.ResponseError res) = showResponse res
+showSubmitException (Gr.ParseError res _) = showResponse res
+
+showResponse :: Res.ResponseMessage a -> IO ()
+showResponse res = showResponseStatus $ Res.status res
+
+showResponseStatus :: Res.ResponseStatus -> IO ()
+showResponseStatus status = mapM_ showKeyValue $ ("message", Res.message status) : textAttributes
+  where
+    textAttributes = justValue =<< (HM.toList $ fmap vToText $ Res.attributes status)
+    vToText (String s) = Just s
+    vToText _ = Nothing
+    justValue (k, Just v) = [(k, v)]
+    justValue (_, Nothing) = []
+    showKeyValue (key, val) = TIO.hPutStrLn stderr (key <> ": " <> val)
+
