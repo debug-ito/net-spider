dec164007f30170bfcadd12779c7af5b9ae2448c
Toshio Ito <debug.ito@gmail.com>
2018-10-28 22:10:49 +0900

    Timestamp: add conversion functions.

--- net-spider/src/NetSpider/Timestamp.hs
+++ net-spider/src/NetSpider/Timestamp.hs
@@ -5,16 +5,26 @@
 --
 -- 
 module NetSpider.Timestamp
-       ( Timestamp(..),
+       ( -- * The type
+         Timestamp(..),
+         -- * Construction
          fromEpochSecond,
          now,
-         addSec
+         -- * Manipulation
+         addSec,
+         -- * Conversion
+         fromZonedTime,
+         fromUTCTime,
+         fromSystemTime,
+         fromLocalTime
        ) where
 
 import Data.Int (Int64)
 import Data.Time.LocalTime
-  ( TimeZone, getZonedTime, ZonedTime(..), zonedTimeToUTC
+  ( TimeZone, getZonedTime, ZonedTime(..), zonedTimeToUTC, LocalTime, localTimeToUTC
   )
+import qualified Data.Time.LocalTime as LocalTime
+import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.System (utcToSystemTime, SystemTime(..))
 
 -- | Timestamp when graph elements are observed.
@@ -38,16 +48,32 @@ fromEpochSecond sec = Timestamp sec Nothing
 
 -- | Get the current system time.
 now :: IO Timestamp
-now = do
-  zt <- getZonedTime
-  return $ Timestamp { epochTime = ztToEpochTime zt,
-                       timeZone = Just $ zonedTimeZone zt
-                     }
+now = fmap fromZonedTime $ getZonedTime
+
+fromZonedTime :: ZonedTime -> Timestamp
+fromZonedTime zt =
+  (fromUTCTime $ zonedTimeToUTC zt) { timeZone = Just $ zonedTimeZone zt }
+
+fromUTCTime :: UTCTime -> Timestamp
+fromUTCTime ut = (fromSystemTime $ utcToSystemTime ut) { timeZone = Just LocalTime.utc }
+
+fromSystemTime :: SystemTime -> Timestamp
+fromSystemTime stime = Timestamp { epochTime = epoch_time,
+                                   timeZone = Nothing
+                                 }
   where
-    ztToEpochTime zt = stimeToEpochTime $ utcToSystemTime $ zonedTimeToUTC zt
-    stimeToEpochTime stime = (systemSeconds stime * 1000)
-                             + fromIntegral (systemNanoseconds stime `div` 1000000)
+    epoch_time = (systemSeconds stime * 1000)
+                 + fromIntegral (systemNanoseconds stime `div` 1000000)
+
+-- | Covert 'LocalTime' to 'Timestamp' assuming it's in UTC time
+-- zone. The 'timeZone' field is 'Nothing'.
+fromLocalTime :: LocalTime -> Timestamp
+fromLocalTime lt = (fromUTCTime $ localTimeToUTC LocalTime.utc lt) { timeZone = Nothing }
 
 -- | Add time difference (in seconds) to the 'Timestamp'.
 addSec :: Int64 -> Timestamp -> Timestamp
 addSec diff ts = ts { epochTime = (+ (diff * 1000)) $ epochTime ts }
+
+-- -- | Parse a string of ISO8601 format into 'Timestamp'.
+-- iso8601parse :: MonadFail m => String -> m Timestamp
+-- iso8601parse = 
