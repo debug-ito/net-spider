484befedc6147d1ab2e536bed72c1346368f459a
Toshio Ito <debug.ito@gmail.com>
2018-10-31 22:03:43 +0900

    Timestamp: implement parseTimestamp. failing.

--- net-spider/src/NetSpider/Timestamp.hs
+++ net-spider/src/NetSpider/Timestamp.hs
@@ -13,21 +13,26 @@ module NetSpider.Timestamp
          -- * Manipulation
          addSec,
          -- * Conversion
-         iso8601Parse,
+         parseTimestamp,
          fromZonedTime,
          fromUTCTime,
          fromSystemTime,
          fromLocalTime
        ) where
 
-import Control.Applicative ((<|>), (<$>))
+import Control.Applicative ((<|>), (<$>), (<*>), (<*), (*>), optional)
+import Data.Char (isDigit)
 import Data.Int (Int64)
+import Data.Time.Calendar (Day, fromGregorian)
 import Data.Time.LocalTime
-  ( TimeZone(timeZoneMinutes), getZonedTime, ZonedTime(..), zonedTimeToUTC, LocalTime, localTimeToUTC
+  ( TimeZone(..), getZonedTime, ZonedTime(..), zonedTimeToUTC, LocalTime(LocalTime), localTimeToUTC,
+    TimeOfDay(TimeOfDay)
   )
 import qualified Data.Time.LocalTime as LocalTime
 import Data.Time.Clock (UTCTime)
 import Data.Time.Clock.System (utcToSystemTime, SystemTime(..))
+import qualified Text.ParserCombinators.ReadP as P
+import Text.Read (readEither)
 
 -- | Timestamp when graph elements are observed.
 data Timestamp =
@@ -76,26 +81,89 @@ fromLocalTime lt = (fromUTCTime $ localTimeToUTC LocalTime.utc lt) { timeZone = 
 addSec :: Int64 -> Timestamp -> Timestamp
 addSec diff ts = ts { epochTime = (+ (diff * 1000)) $ epochTime ts }
 
--- | Parse a string of ISO8601 format into 'Timestamp'.
+-- | Parse a string into 'Timestamp'. The format is like ISO8601 with
+-- a little relaxation.
 --
 -- >>> let timeAndOffset ts = (epochTime ts, fmap timeZoneMinutes $ timeZone ts)
--- >>> fmap timeAndOffset $ iso8601Parse "2018-10-11T11:20:10"
--- Just (1539256810000, Nothing)
--- >>> iso8601Parse "2018-10-11 11:20:10"
--- Just (1539256810000, Nothing)
--- >>> iso8601Parse "2015-03-23 03:33Z"
--- Just (1427081580000, Just 0)
--- >>> iso8601Parse "1999-01-05 20:34:44.211+09:00"
--- Just (915536084211, Just 540)
--- >>> iso8601Parse "2007-08-20T22:25-07:00"
--- Just (1187673900000, Just (-420))
-iso8601Parse :: String -> Maybe Timestamp
-iso8601Parse s = (fromZonedTime <$> parseZoned s)
-                 <|> (fromUTCTime <$> parseUTC s)
-                 <|> (fromLocalTime <$> parseLocal s)
+-- >>> fmap timeAndOffset $ parseTimestamp "2018-10-11T11:20:10"
+-- Just (1539256810000,Nothing)
+-- >>> fmap timeAndOffset $ parseTimestamp "2018-10-11 11:20:10"
+-- Just (1539256810000,Nothing)
+-- >>> fmap timeAndOffset $ parseTimestamp "2015-03-23 03:33Z"
+-- Just (1427081580000,Just 0)
+-- >>> fmap timeAndOffset $ parseTimestamp "1999-01-05 20:34:44.211+09:00"
+-- Just (915536084211,Just 540)
+-- >>> fmap timeAndOffset $ parseTimestamp "2007/08/20T22:25-07:00"
+-- Just (1187673900000,Just (-420))
+parseTimestamp :: String -> Maybe Timestamp
+parseTimestamp s = case P.readP_to_S parserTimestamp s of
+                    ((ret, _) : _) -> Just ret
+                    _ -> Nothing
+
+parserTimestamp :: P.ReadP Timestamp
+parserTimestamp = do
+  day <- parserDay <* delim
+  time <- parserTime
+  mtz <- optional (parserUTC <|> parserOffset)
+  let ltime = LocalTime day time
+  case mtz of
+   Nothing -> return $ fromLocalTime ltime
+   Just tz -> return $ fromZonedTime $ ZonedTime ltime tz
   where
-    parserBase = readPTime True defaultTimeLocale
-    parserYMD = parserBase "%Y-%m-%d"
-    parserZoned = undefined
-    -- TODO: うーん、ReadPを直接使うほうがラクそうな気がする。
+    delim = P.choice $ map P.char " T"
+
+parserRead :: Read a => String -> P.ReadP a
+parserRead input = either fail return $ readEither input
+
+parserDec :: Read a => P.ReadP a
+parserDec = parserRead =<< P.munch1 isDigit
+
+parserDay :: P.ReadP Day
+parserDay = fromGregorian
+            <$> (parserDec <* delim)
+            <*> (parserDec <* delim)
+            <*> parserDec
+  where
+    delim = P.choice $ map P.char "-/"
+
+parserTime :: P.ReadP TimeOfDay
+parserTime = TimeOfDay
+             <$> parserDec
+             <*> (delim *> parserDec)
+             <*> ((delim *> parserDec) <|> pure 0)
+  where
+    delim = P.char ':'
+
+
+parserUTC :: P.ReadP TimeZone
+parserUTC = do
+  s <- P.get
+  case s of
+   'Z' -> return LocalTime.utc
+   c -> fail ("Not a UTC symbol: " ++ show c)
+
+data OffsetSign = OffsetPlus
+                | OffsetMinus
+                deriving (Show,Eq,Ord,Enum,Bounded)
+
+parserOffset :: P.ReadP TimeZone
+parserOffset = offsetToTz <$> osign <*> (parserDec <* delim) <*> parserDec
+  where
+    osign = do
+      s <- P.get
+      case s of
+       '+' -> return OffsetPlus
+       '-' -> return OffsetMinus
+       c -> fail ("Not a sign symbol: " ++ show c)
+    delim = optional $ P.char ':'
+
+offsetToTz :: OffsetSign -> Int -> Int -> TimeZone
+offsetToTz osign h m = TimeZone { timeZoneMinutes = intsign * (h * 60 + m),
+                                  timeZoneSummerOnly = False,
+                                  timeZoneName = ""
+                                }
+  where
+    intsign = case osign of
+      OffsetPlus -> 1
+      OffsetMinus -> -1
 
