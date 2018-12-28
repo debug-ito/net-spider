11863ae361b9f4f6f04931d08894e9d6496bb694
Toshio Ito <debug.ito@gmail.com>
2018-12-28 21:58:41 +0900

    Pangraph: add preliminary signature for basic converters.

--- net-spider-pangraph/src/NetSpider/Pangraph.hs
+++ net-spider-pangraph/src/NetSpider/Pangraph.hs
@@ -6,3 +6,13 @@
 module NetSpider.Pangraph
        () where
 
+import qualified Pangraph as P
+
+makeVertex :: SnapshotNode n na -> P.Vertex
+makeVertex = undefined
+
+makeEdge :: SnapshotLink n fla -> P.Edge
+makeEdge = undefined
+
+makePangraph :: [SnapshotNode n na] -> [SnapshotLink n fla] -> Maybe P.Pangraph
+makePangraph = undefined
