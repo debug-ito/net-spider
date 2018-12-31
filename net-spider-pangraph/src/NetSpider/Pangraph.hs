40efb23fbee0fd50870ba2540e2876f3054a467b
Toshio Ito <debug.ito@gmail.com>
2018-12-31 14:33:00 +0900

    Pangraph: add timestampAttributes function and its doctest.

--- net-spider-pangraph/src/NetSpider/Pangraph.hs
+++ net-spider-pangraph/src/NetSpider/Pangraph.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
 -- |
 -- Module: NetSpider.Pangraph
 -- Description:
@@ -9,19 +9,48 @@ module NetSpider.Pangraph
          makeVertex,
          makeEdge,
          ToAttributes(..),
-         spackBS
+         spackBS,
+         timestampAttributes
        ) where
 
 import Data.ByteString (ByteString)
 import Data.Text (pack)
 import Data.Text.Encoding (encodeUtf8)
+import Data.Time.LocalTime (TimeZone(..))
 import NetSpider.Snapshot
   ( SnapshotNode, SnapshotLink,
     nodeId, nodeAttributes,
     sourceNode, destinationNode, linkAttributes
   )
+import NetSpider.Timestamp (Timestamp(..), showEpochTime)
 import qualified Pangraph as P
 
+-- | Make Pangraph 'P.Attribute's from 'Timestamp'.
+--
+-- >>> import NetSpider.Timestamp (fromS)
+-- >>> timestampAttributes $ fromS "2018-10-11T11:23:05"
+-- [("@timestamp","1539256985000")]
+-- >>> timestampAttributes $ fromS "2018-09-23T08:48:52+09:00"
+-- [("@timestamp","1537660132000"),("@tz_offset_min","540"),("@tz_summer_only","False"),("@tz_name","")]
+-- >>> timestampAttributes $ fromS "2018-12-30T22:00:12.109-04:00"
+-- [("@timestamp","1546221612109"),("@tz_offset_min","-240"),("@tz_summer_only","False"),("@tz_name","")]
+timestampAttributes :: Timestamp -> [P.Attribute]
+timestampAttributes ts = epoch : tz_attrs
+  where
+    epoch = ("@timestamp", encodeUtf8 $ showEpochTime ts)
+    tz_attrs = case timeZone ts of
+      Nothing -> []
+      Just tz -> [ ("@tz_offset_min", spackBS $ timeZoneMinutes tz),
+                   ("@tz_summer_only", spackBS $ timeZoneSummerOnly tz),
+                   ("@tz_name", encodeUtf8 $ pack $ timeZoneName tz)
+                 ]
+
+-- | Make Pangraph 'P.Vertex' from 'SnapshotNode'.
+--
+-- Node attributes (@na@) is converted to attributes of
+-- 'P.Vertex'. 'nodeTimestamp' is converted by 'timestampAttributes',
+-- if present. 'isOnBoundary' is stored as \"\@is_on_boundary\"
+-- attribute, if present.  respectively.
 makeVertex :: (Show n, ToAttributes na) => SnapshotNode n na -> P.Vertex
 makeVertex sn = P.makeVertex vid attrs
   where
@@ -31,6 +60,12 @@ makeVertex sn = P.makeVertex vid attrs
       Just a -> toAttributes a
     -- how about timestamp and isBoundary?
 
+-- | Make Pangraph 'P.Edge' from 'SnapshotLink'.
+--
+-- Link attributes (@la@) is converted to attributes of
+-- 'P.Edge'. 'linkTimestamp' is converted by
+-- 'timestampAttributes'. 'isDirected' is stored as \"@is_directed\"
+-- attribute..
 makeEdge :: (Show n, ToAttributes la) => SnapshotLink n la -> P.Edge
 makeEdge sl = P.makeEdge (src, dest) attrs
   where
