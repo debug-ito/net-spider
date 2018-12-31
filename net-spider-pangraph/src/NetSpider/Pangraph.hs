dd642737c977e14c82485eb403a9f089ef82cb3a
Toshio Ito <debug.ito@gmail.com>
2018-12-31 12:06:13 +0900

    Pangraph: add ToAttributes class.

--- net-spider-pangraph/src/NetSpider/Pangraph.hs
+++ net-spider-pangraph/src/NetSpider/Pangraph.hs
@@ -1,11 +1,20 @@
+{-# LANGUAGE FlexibleInstances #-}
 -- |
 -- Module: NetSpider.Pangraph
 -- Description:
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module NetSpider.Pangraph
-       () where
+       ( makePangraph,
+         makeVertex,
+         makeEdge,
+         ToAttributes(..)
+       ) where
 
+import Data.ByteString (ByteString)
+import Data.Text (pack)
+import Data.Text.Encoding (encodeUtf8)
+import NetSpider.Snapshot (SnapshotNode, SnapshotLink)
 import qualified Pangraph as P
 
 makeVertex :: SnapshotNode n na -> P.Vertex
@@ -16,3 +25,19 @@ makeEdge = undefined
 
 makePangraph :: [SnapshotNode n na] -> [SnapshotLink n fla] -> Maybe P.Pangraph
 makePangraph = undefined
+
+spack :: Show a => a -> ByteString
+spack = encodeUtf8 . pack . show
+
+-- | Data types that can be converted into a list of Pangraph
+-- 'P.Attribute's.
+class ToAttributes a where
+  toAttributes :: a -> [P.Attribute]
+
+-- | No attribute.
+instance ToAttributes () where
+  toAttributes () = []
+
+-- | Show key and value to make 'P.Attribute'.
+instance (Show k, Show v) => ToAttributes [(k,v)] where
+  toAttributes = map (\(k, v) -> (spack k, spack v))
