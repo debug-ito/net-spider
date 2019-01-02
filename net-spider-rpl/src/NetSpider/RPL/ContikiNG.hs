a0ff4d23c162739fd79b5c657f9cb4b5b05bed4b
Toshio Ito <debug.ito@gmail.com>
2019-01-02 22:11:45 +0900

    rpl ContikiNG: writing pNeighbor parser.

--- net-spider-rpl/src/NetSpider/RPL/ContikiNG.hs
+++ net-spider-rpl/src/NetSpider/RPL/ContikiNG.hs
@@ -5,9 +5,11 @@
 --
 -- 
 module NetSpider.RPL.ContikiNG
-  ( pLocalNode
+  ( pLocalNode,
+    pNeighbor
   ) where
 
+import Control.Applicative ((<|>))
 import Control.Monad (void)
 import Data.Char (isDigit)
 import Data.Monoid ((<>))
@@ -20,9 +22,6 @@ import Text.Read (readEither)
 import qualified NetSpider.RPL.Local as Local
 
 type Parser = P.ReadP
-
-pHeading :: Parser ()
-pHeading = void $ P.string "nbr: own state, addr "
 
 isAddressChar :: Char -> Bool
 isAddressChar c = c `elem` charset
@@ -37,12 +36,18 @@ pAddress = fromS =<< P.munch1 isAddressChar
         Nothing -> fail ("Invalid IPv6 address: " <> str)
         Just addr -> return addr
 
+pCompactAddress :: Parser IPv6
+pCompactAddress = undefined -- TODO
+
+-- neighbor address can be logged in a "compact" form.
+-- https://github.com/contiki-ng/contiki-ng/blob/develop/os/sys/log.c
+
 pRead :: Read a => String -> Parser a
 pRead = either fail return . readEither
 
 pLocalNode :: Parser (IPv6, Local.LocalNode)
 pLocalNode = do
-  pHeading
+  void $ P.string "nbr: own state, addr "
   addr <- pAddress
   void $ P.string ", DAG state: "
   void $ P.munch (\c -> c /= ',')
@@ -58,3 +63,39 @@ pLocalNode = do
   void $ P.munch isDigit -- TODO: store DIO interval
   return (addr, Local.LocalNode { Local.rank = rank })
 
+pExpectChar :: Char -> Parser Bool
+pExpectChar exp_c = fmap (== exp_c) $ P.get
+
+pNeighbor :: Parser (IPv6, Local.LocalLink)
+pNeighbor = do
+  void $ P.string "nbr: "
+  neighbor_addr <- pCompactAddress <|> pAddress
+  P.skipSpaces
+  neighbor_rank <- pRead =<< P.munch1 isDigit
+  void $ P.string ", "
+  P.skipSpaces
+  metric <- pRead =<< P.munch1 isDigit
+  void $ P.string " => "
+  P.skipSpaces
+  void $ P.munch isDigit -- rank_via_neighbor
+  void $ P.string " -- "
+  P.skipSpaces
+  void $ P.munch isDigit -- freshness
+  void $ P.string " "
+  void $ pExpectChar 'r'
+  void $ pExpectChar 'b'
+  acceptable <- pExpectChar 'a'
+  void $ pExpectChar 'f'
+  preferred <- pExpectChar 'p'
+  return ( neighbor_addr,
+           Local.LocalLink
+           { Local.neighborType = if preferred
+                                  then Local.PreferredParent
+                                  else if acceptable
+                                       then Local.ParentCandidate
+                                       else Local.OtherNeighbor,
+             Local.neighborRank = neighbor_rank,
+             Local.metric = metric,
+             Local.rssi = Nothing -- TODO: How should we get this?
+           }
+         )
