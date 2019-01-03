d33b7d706f714d8851b3023da3b4ab27357aa3cd
Toshio Ito <debug.ito@gmail.com>
2019-01-03 11:37:19 +0900

    rpl: ContikiNG: implement pCompactAddress.

--- net-spider-rpl/src/NetSpider/RPL/ContikiNG.hs
+++ net-spider-rpl/src/NetSpider/RPL/ContikiNG.hs
@@ -11,9 +11,11 @@ module NetSpider.RPL.ContikiNG
 
 import Control.Applicative ((<|>))
 import Control.Monad (void)
-import Data.Char (isDigit)
+import Data.Bits (shift)
+import Data.Char (isDigit, isHexDigit)
 import Data.Monoid ((<>))
 import Data.Text (pack)
+import Data.Word (Word16)
 import Net.IPv6 (IPv6)
 import qualified Net.IPv6 as IPv6
 import qualified Text.ParserCombinators.ReadP as P
@@ -24,9 +26,7 @@ import qualified NetSpider.RPL.Local as Local
 type Parser = P.ReadP
 
 isAddressChar :: Char -> Bool
-isAddressChar c = c `elem` charset
-  where
-    charset = ":" ++ ['0' .. '9'] ++ ['a' .. 'f'] ++ ['A' .. 'F']
+isAddressChar c = isHexDigit c || c == ':'
 
 pAddress :: Parser IPv6
 pAddress = fromS =<< P.munch1 isAddressChar
@@ -36,8 +36,41 @@ pAddress = fromS =<< P.munch1 isAddressChar
         Nothing -> fail ("Invalid IPv6 address: " <> str)
         Just addr -> return addr
 
+data CompactID = CNodeID !Int
+               | CNodeAddress !Word16
+               deriving (Show,Eq,Ord)
+
+makeCompactAddress :: CompactID -> IPv6
+makeCompactAddress cid =
+  case cid of
+    CNodeID nid -> IPv6.fromWord32s 0 0 0 (fromIntegral nid)
+    CNodeAddress addr -> IPv6.fromWord16s 0 0 0 0 0 0 0 addr
+
+pHexWord16 :: String -> Parser Word16
+pHexWord16 input = go 0 input
+  where
+    go acc [] = return acc
+    go acc (c:rest) = do
+      c_num <- parseC
+      go ((acc `shift` 8) + c_num) rest
+      where
+        diffWord a b = fromIntegral (fromEnum a - fromEnum b)
+        parseC = if c >= '0' && c <= '9'
+                 then return $ diffWord c '0'
+                 else if c >= 'a' && c <= 'f'
+                      then return $ diffWord c 'a'
+                      else if c >= 'A' && c <= 'F'
+                           then return $ diffWord c 'A'
+                           else fail ("Invalid hex number: " <> input)
+
+pCompactID :: Parser CompactID
+pCompactID = (fmap CNodeID $ pRead =<< P.count 3 (P.satisfy isDigit))
+             <|> (fmap CNodeAddress $ pHexWord16 =<< P.count 4 (P.satisfy isHexDigit))
+
 pCompactAddress :: Parser IPv6
-pCompactAddress = undefined -- TODO
+pCompactAddress = do
+  void $ P.string "6G-"  -- expecting unicast global address
+  fmap makeCompactAddress $ pCompactID
 
 -- neighbor address can be logged in a "compact" form.
 -- https://github.com/contiki-ng/contiki-ng/blob/develop/os/sys/log.c
