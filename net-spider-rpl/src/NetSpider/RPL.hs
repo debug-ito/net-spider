707df4ec0b04f1d1598069b2719ff5c38782eb15
Toshio Ito <debug.ito@gmail.com>
2019-01-26 22:31:03 +0900

    RPL: re-export component modules.

--- net-spider-rpl/src/NetSpider/RPL.hs
+++ net-spider-rpl/src/NetSpider/RPL.hs
@@ -5,6 +5,12 @@
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module NetSpider.RPL
-       ( 
+       ( module NetSpider.RPL.FindingID,
+         module NetSpider.RPL.Local,
+         module NetSpider.RPL.SR
        ) where
+
+import NetSpider.RPL.FindingID
+import NetSpider.RPL.Local
+import NetSpider.RPL.SR
 
