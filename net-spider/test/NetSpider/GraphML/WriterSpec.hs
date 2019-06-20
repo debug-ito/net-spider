664fa6c94c355131df2d92183d280864ad8e6790
Toshio Ito <debug.ito@gmail.com>
2019-06-20 19:18:20 +0900

    GraphML.WriterSpec: add test case with timestamp with time zone.

--- net-spider/test/NetSpider/GraphML/WriterSpec.hs
+++ net-spider/test/NetSpider/GraphML/WriterSpec.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module NetSpider.GraphML.WriterSpec (main,spec) where
 
+import Data.Maybe (fromJust)
 import Data.Text (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.IO as TL
 import NetSpider.Snapshot.Internal
   ( SnapshotNode(..), SnapshotLink(..)
   )
-import NetSpider.Timestamp (fromEpochMillisecond)
+import NetSpider.Timestamp (fromEpochMillisecond, fromS)
 import Test.Hspec
 
 import NetSpider.GraphML.Writer (writeGraphML)
@@ -19,7 +20,8 @@ spec :: Spec
 spec = do
   describe "writeGraphML" $ do
     specify "no attribute, directed and undirected mixed, node id escaped" $ do
-      let nodes :: [SnapshotNode Text ()]
+      let time_with_tz = fromS "2018-09-23T08:48:52+09:00"
+          nodes :: [SnapshotNode Text ()]
           nodes = [ SnapshotNode
                     { _nodeId = "\"the root\"",
                       _isOnBoundary = False,
@@ -35,7 +37,7 @@ spec = do
                     SnapshotNode
                     { _nodeId = "<child>",
                       _isOnBoundary = False,
-                      _nodeTimestamp = Just $ fromEpochMillisecond 120,
+                      _nodeTimestamp = Just $ time_with_tz,
                       _nodeAttributes = Just ()
                     }
                   ]
@@ -51,7 +53,7 @@ spec = do
                     { _sourceNode = "<child>",
                       _destinationNode = "\"the root\"",
                       _isDirected = False,
-                      _linkTimestamp = fromEpochMillisecond 120,
+                      _linkTimestamp = time_with_tz,
                       _linkAttributes = ()
                     }
                   ]
@@ -60,8 +62,15 @@ spec = do
                        "<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"",
                        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"",
                        "xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">",
-                       "<key id=\"d0\" for=\"all\" attr.name=\"@timestamp\" attr.type=\"long\"/>",
+                       "<key id=\"d0\" for=\"node\" attr.name=\"@timestamp\" attr.type=\"long\"/>",
                        "<key id=\"d1\" for=\"node\" attr.name=\"@is_on_boundary\" attr.type=\"boolean\"/>",
+                       "<key id=\"d2\" for=\"node\" attr.name=\"@tz_offset_min\" attr.type=\"int\"/>",
+                       "<key id=\"d3\" for=\"node\" attr.name=\"@tz_summer_only\" attr.type=\"boolean\"/>",
+                       "<key id=\"d4\" for=\"node\" attr.name=\"@tz_name\" attr.type=\"string\"/>",
+                       "<key id=\"d5\" for=\"edge\" attr.name=\"@timestamp\" attr.type=\"long\"/>",
+                       "<key id=\"d6\" for=\"edge\" attr.name=\"@tz_offset_min\" attr.type=\"int\"/>",
+                       "<key id=\"d7\" for=\"edge\" attr.name=\"@tz_summer_only\" attr.type=\"boolean\"/>",
+                       "<key id=\"d8\" for=\"edge\" attr.name=\"@tz_name\" attr.type=\"string\"/>",
                        "<graph edgedefault=\"undirected\">",
                        "  <node id=\"&quot;the root&quot;\">",
                        "    <data key=\"d0\">100</data>",
@@ -71,14 +80,20 @@ spec = do
                        "    <data key=\"d1\">true</data>",
                        "  </node>",
                        "  <node id=\"&lt;child&gt;\">",
-                       "    <data key=\"d0\">120</data>",
+                       "    <data key=\"d0\">1537660132000</data>",
+                       "    <data key=\"d2\">540</data>",
+                       "    <data key=\"d3\">false</data>",
+                       "    <data key=\"d4\"></data>",
                        "    <data key=\"d1\">true</data>",
                        "  </node>",
                        "  <edge source=\"&quot;the root&quot;\" target=\"☃\" directed=\"true\">",
-                       "    <data key=\"d0\">100</data>",
+                       "    <data key=\"d5\">100</data>",
                        "  </edge>",
                        "  <edge source=\"&lt;child&gt;\" target=\"&quot;the root&quot;\" directed=\"false\">",
-                       "    <data key=\"d0\">120</data>",
+                       "    <data key=\"d5\">1537660132000</data>",
+                       "    <data key=\"d6\">540</data>",
+                       "    <data key=\"d7\">false</data>",
+                       "    <data key=\"d8\"></data>",
                        "  </edge>",
                        "</graph>"
                      ]
