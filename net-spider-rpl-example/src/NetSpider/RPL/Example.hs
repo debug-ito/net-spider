f18d9d33c63831f8e1ce31c176f4a4cf6998c9eb
Toshio Ito <debug.ito@gmail.com>
2019-07-14 11:02:46 +0900

    rpl-example: refactor clearDB outside.
    If there was no dio_nodes, it did not run clear.

--- net-spider-rpl-example/src/NetSpider/RPL/Example.hs
+++ net-spider-rpl-example/src/NetSpider/RPL/Example.hs
@@ -23,7 +23,8 @@ import NetSpider.Input
     connectWith, close, addFoundNode, clearAll,
     FoundNode(subjectNode, foundAt, neighborLinks, nodeAttributes),
     FoundLink(targetNode, linkAttributes),
-    LinkAttributes, NodeAttributes
+    LinkAttributes, NodeAttributes,
+    Spider
   )
 import NetSpider.Output
   ( getSnapshot,
@@ -44,22 +45,70 @@ import NetSpider.RPL.ContikiNG (parseFile, pSyslogHead)
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
 
+main :: IO ()
+main = do
+  filenames <- getArgs
+  
+  -- Read DIO and DAO FoundNodes. It might take a long time to insert
+  -- a lot of FoundNodes, so this example inserts only the latest
+  -- FoundNode per node into the net-spider database.
+  (dio_nodes, dao_nodes) <- fmap (concatPairs . map (filterPairs getLatestForEachNode))
+                            $ mapM loadFile filenames
+  forM_ dio_nodes printDIONode
+  forM_ dao_nodes printDAONode
+  clearDB
+  dio_graph <- if null dio_nodes
+               then return ([], [])
+               else let dio_query = (DIO.dioDefQuery [subjectNode (dio_nodes !! 0)])
+                    in putNodes dio_query dio_nodes
+  dao_graph <- if null dao_nodes
+               then return ([], [])
+               else let dao_input = sortDAONodes dao_nodes
+                        dao_query = (DAO.daoDefQuery [subjectNode (dao_input !! 0)])
+                          { timeInterval = 0 `secUpTo` foundAt (dao_input !! 0)
+                          }
+                    in putNodes dao_query dao_input
+  let com_graph = RPL.combineGraphs dio_graph dao_graph
+  TLIO.putStr $ writeGraphML com_graph
+
+-- | Read a Contiki-NG log file, parse it with
+-- 'NetSpider.RPL.ContikiNG.parseFile' to get 'FoundNodeDIO' and
+-- 'FoundNodeDAO'.
+loadFile :: FilePath
+         -> IO ([FoundNodeDIO], [FoundNodeDAO])
+loadFile file = do
+  hPutStrLn stderr ("---- Loading " <> file)
+  (dio_nodes, dao_nodes) <- parseFile phead file
+  hPutStrLn stderr ((show $ length dio_nodes) <> " DIO Nodes")
+  hPutStrLn stderr ((show $ length dao_nodes) <> " DAO Nodes")
+  return (dio_nodes, dao_nodes)
+  where
+    phead = pSyslogHead 2019 Nothing
+
+clearDB :: IO ()
+clearDB = do
+  hPutStrLn stderr "---- Clear graph database"
+  bracket (connectWith defConfig) close doClear
+  where
+    doClear :: Spider FindingID () () -> IO ()
+    doClear = clearAll
+  
+-- | Put (insert) the given 'FoundNode's into the net-spider database
+-- and get a 'SnapshotGraph' by the given 'Query'.
 putNodes :: (LinkAttributes fla, NodeAttributes na)
-         => Bool
-         -> Query FindingID na fla sla
+         => Query FindingID na fla sla
          -> [FoundNode FindingID na fla]
          -> IO (SnapshotGraph FindingID na sla)
-putNodes do_clear query input_nodes = do
+putNodes query input_nodes = do
   bracket (connectWith defConfig) close $ \sp -> do
-    when do_clear $ do
-      hPutStrLn stderr "---- Clear graph database"
-      clearAll sp
     hPutStrLn stderr ("---- Add " <> (show $ length $ input_nodes) <> " FoundNodes")
     forM_ (zip input_nodes ([0 ..] :: [Integer])) $ \(input_node, index) -> do
       when ((index `mod` 100) == 0) $ hPutStrLn stderr ("Add node [" <> show index <> "]")
       addFoundNode sp input_node
     hPutStrLn stderr "Add done"
     getSnapshot sp query
+
+---- Print FoundNodes for debug
 
 printDIONode :: FoundNodeDIO -> IO ()
 printDIONode fn = do
@@ -84,16 +133,7 @@ printDAONode fn = do
         Nothing -> ""
         Just n -> ", route_num " <> (pack $ show n)
 
-loadFile :: FilePath
-         -> IO ([FoundNodeDIO], [FoundNodeDAO])
-loadFile file = do
-  hPutStrLn stderr ("---- Loading " <> file)
-  (dio_nodes, dao_nodes) <- parseFile phead file
-  hPutStrLn stderr ((show $ length dio_nodes) <> " DIO Nodes")
-  hPutStrLn stderr ((show $ length dao_nodes) <> " DAO Nodes")
-  return (dio_nodes, dao_nodes)
-  where
-    phead = pSyslogHead 2019 Nothing
+---- General utility functions.
 
 filterPairs :: (forall na la . [FoundNode FindingID na la] -> [FoundNode FindingID na la])
             -> ([FoundNode FindingID na1 la1], [FoundNode FindingID na2 la2])
@@ -105,6 +145,9 @@ concatPairs [] = ([],[])
 concatPairs ((as, bs) : rest) = (as ++ rest_as, bs ++ rest_bs)
   where
     (rest_as, rest_bs) = concatPairs rest
+
+
+---- Filter for FoundNode.
 
 type NodeMap n = HashMap FindingID [n]
 
@@ -125,25 +168,8 @@ getLatestNodes nm = concat $ HM.elems $ fmap filterLatest nm
 getLatestForEachNode :: [FoundNode FindingID na la] -> [FoundNode FindingID na la]
 getLatestForEachNode = getLatestNodes . collectNodes
 
+
+---- FoundNode utility
+
 sortDAONodes :: [FoundNodeDAO] -> [FoundNodeDAO]
 sortDAONodes = reverse . sortOn (DAO.daoRouteNum . nodeAttributes)
-
-main :: IO ()
-main = do
-  filenames <- getArgs
-  (dio_nodes, dao_nodes) <- fmap (concatPairs . map (filterPairs getLatestForEachNode)) $ mapM loadFile filenames
-  forM_ dio_nodes printDIONode
-  forM_ dao_nodes printDAONode
-  dio_graph <- if null dio_nodes
-               then return ([], [])
-               else let dio_query = (DIO.dioDefQuery [subjectNode (dio_nodes !! 0)])
-                    in putNodes True dio_query dio_nodes
-  dao_graph <- if null dao_nodes
-               then return ([], [])
-               else let dao_input = sortDAONodes dao_nodes
-                        dao_query = (DAO.daoDefQuery [subjectNode (dao_input !! 0)])
-                          { timeInterval = 0 `secUpTo` foundAt (dao_input !! 0)
-                          }
-                    in putNodes False dao_query dao_input
-  let com_graph = RPL.combineGraphs dio_graph dao_graph
-  TLIO.putStr $ writeGraphML com_graph
