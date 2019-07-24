c54d0634757fd238aa18d58683aa0774e18be5ae
Toshio Ito <toshio9.ito@toshiba.co.jp>
2019-07-24 12:07:15 +0900

    cli: re-implement Command with Action type.
    This is more flexible and extensible.

--- net-spider-cli/src/NetSpider/CLI/Command.hs
+++ net-spider-cli/src/NetSpider/CLI/Command.hs
@@ -7,35 +7,86 @@
 module NetSpider.CLI.Command
   ( -- * Top-level parser
     parserCommand,
-    Config(..),
+    execCommand,
+    -- * Actions
+    actionClear,
+    actionGetSnapshot,
+    actionSnapshotGraphML,
+    Action(..),
+    afterAction,
     Command(..),
-    -- * \"snapshot\" query
-    parserSnapshotQuery,
   ) where
 
 import Control.Applicative ((<$>), (<*>), pure)
+import Data.Aeson (ToJSON)
+import Data.Hashable (Hashable)
+import Data.Greskell.GraphSON (FromGraphSON)
 import Data.Monoid (mconcat)
+import qualified Data.Text.Lazy.IO as TLIO
+import NetSpider.Graph (LinkAttributes, NodeAttributes)
+import NetSpider.GraphML.Writer (writeGraphML, ToNodeID, ToAttributes)
 import qualified NetSpider.Query as Q
+import NetSpider.Snapshot (SnapshotGraph)
+import NetSpider.Spider (getSnapshot)
 import qualified Options.Applicative as Opt
 
-import NetSpider.CLI.Snapshot (Config, parserSnapshotQuery)
-import NetSpider.CLI.Spider (parserSpiderConfig, SpiderConfig)
+import NetSpider.CLI.Spider
+  ( parserSpiderConfig, SpiderConfig,
+    clearDatabase, withSpider
+  )
+import qualified NetSpider.CLI.Snapshot as Snapshot
 
--- | Subcommand specified in CLI.
-data Command n na fla sla =
-    CmdClean
-  | CmdSnapshot (Q.Query n na fla sla)
-  | CmdInput
+newtype Action n na fla a =
+  Action { unAction :: Opt.Parser (SpiderConfig n na fla -> IO a) }
 
--- | Top-level command-line parser.
-parserCommand :: Config n na fla sla -> Opt.Parser (SpiderConfig n na fla, Command n na fla sla)
-parserCommand conf = (,) <$> parserSpiderConfig <*> pCommand
+instance Functor (Action n na fla) where
+  fmap f (Action a) = Action $ (fmap . fmap . fmap) f a
+
+afterAction :: (a -> IO b) -> Action n na fla a -> Action n na fla b
+afterAction then_action (Action a) = Action $ fmap modifyAction a
   where
-    pCommand = Opt.hsubparser $ mconcat
-               [ Opt.command "clean"
-                 $ Opt.info (pure CmdClean) (Opt.progDesc "Clean all the content in the database."),
-                 Opt.command "snapshot"
-                 $ Opt.info (fmap CmdSnapshot $ parserSnapshotQuery conf) (Opt.progDesc "Get a SnapshotGraph from the database."),
-                 Opt.command "input"
-                 $ Opt.info (pure CmdInput) (Opt.progDesc "Input local findings to the database.")
+    modifyAction orig_action sconf = then_action =<< orig_action sconf
+
+actionClear :: Action n na fla ()
+actionClear = Action $ pure clearDatabase
+
+actionGetSnapshot :: (FromGraphSON n, ToJSON n, Ord n, Hashable n, Show n, LinkAttributes fla, NodeAttributes na)
+                  => Snapshot.Config n na fla sla
+                  -> Action n na fla (SnapshotGraph n na sla)
+actionGetSnapshot conf = Action $ fmap doGetSnapshot $ Snapshot.parserSnapshotQuery conf
+  where
+    doGetSnapshot query sconf = withSpider sconf $ \sp -> getSnapshot sp query
+
+actionSnapshotGraphML :: (FromGraphSON n, ToJSON n, Ord n, Hashable n, Show n, LinkAttributes fla, NodeAttributes na, ToNodeID n, ToAttributes na, ToAttributes sla)
+                      => Snapshot.Config n na fla sla
+                      -> Action n na fla ()
+actionSnapshotGraphML conf = afterAction printGraphML $ actionGetSnapshot conf
+  where
+    printGraphML = TLIO.putStrLn . writeGraphML
+
+data Command n na fla =
+  Command
+  { cmdName :: String,
+    cmdDescription :: String,
+    cmdAction :: Action n na fla ()
+  }
+
+parserCommand :: [Command n na fla] -> Opt.Parser (SpiderConfig n na fla, SpiderConfig n na fla -> IO ())
+parserCommand commands = (,) <$> parserSpiderConfig <*> pCommand
+  where
+    pCommand = Opt.hsubparser $ mconcat $ map toCommandMod commands
+    toCommandMod cmd =
+      Opt.command (cmdName cmd) $ Opt.info (unAction $ cmdAction cmd) (Opt.progDesc $ cmdDescription cmd)
+
+execCommand :: [Command n na fla] -> IO ()
+execCommand commands = exec $ parserCommand commands
+
+exec :: Opt.Parser (a, a -> IO ()) -> IO ()
+exec p = do
+  (a, act) <- Opt.execParser $ Opt.info p info_mod
+  act a
+  where
+    info_mod = mconcat
+               [ Opt.fullDesc,
+                 Opt.progDesc "Command-line front-end for net-spider"
                ]
--- net-spider-cli/src/NetSpider/CLI/Exec.hs
+++ net-spider-cli/src/NetSpider/CLI/Exec.hs
@@ -5,48 +5,27 @@
 --
 -- 
 module NetSpider.CLI.Exec
-  ( mainBy,
-    execCommand
+  ( mainBy
   ) where
 
-import qualified Data.Text.Lazy.IO as TLIO
 import Data.Aeson (ToJSON)
 import Data.Hashable (Hashable)
 import Data.Greskell.GraphSON (FromGraphSON)
-import Data.Monoid (mconcat)
 import NetSpider.Graph (LinkAttributes, NodeAttributes)
-import NetSpider.GraphML.Writer (writeGraphML, ToNodeID, ToAttributes)
-import NetSpider.Spider (getSnapshot)
-import qualified Options.Applicative as Opt
+import NetSpider.GraphML.Writer (ToNodeID, ToAttributes)
 
-import NetSpider.CLI.Spider
-  ( SpiderConfig, clearDatabase, withSpider
-  )
 import NetSpider.CLI.Command
-  ( Command(..), parserCommand, Config(..)
+  ( Command(..), execCommand,
+    actionClear, actionSnapshotGraphML
   )
+import qualified NetSpider.CLI.Snapshot as Snapshot
 
 -- | The main routine controlled by the given 'Config'.
 mainBy :: (FromGraphSON n, ToJSON n, Ord n, Hashable n, Show n, LinkAttributes fla, NodeAttributes na, ToNodeID n, ToAttributes na, ToAttributes sla)
-       => Config n na fla sla
+       => Snapshot.Config n na fla sla
        -> IO ()
-mainBy conf = do
-  (sconf, cmd) <- Opt.execParser $ Opt.info (parserCommand conf) info_mod
-  execCommand conf sconf cmd
+mainBy conf = execCommand commands
   where
-    info_mod = mconcat
-               [ Opt.fullDesc,
-                 Opt.progDesc "Command-line front-end for net-spider"
+    commands = [ Command "clear" "Clear the database." actionClear,
+                 Command "snapshot" "Get a snapshot graph." $ actionSnapshotGraphML conf
                ]
-
--- | Execute the 'Command' obtained from CLI.
-execCommand :: (FromGraphSON n, ToJSON n, Ord n, Hashable n, Show n, LinkAttributes fla, NodeAttributes na, ToNodeID n, ToAttributes na, ToAttributes sla)
-            => Config n na fla sla
-            -> SpiderConfig n na fla
-            -> Command n na fla sla
-            -> IO ()
-execCommand _ sconf CmdClean = clearDatabase sconf
-execCommand _ sconf (CmdSnapshot query) = withSpider sconf $ \sp -> do
-  graph <- getSnapshot sp query
-  TLIO.putStrLn $ writeGraphML graph -- TODO: add option to select output format.
-execCommand conf sconf CmdInput = return () -- TODO: make it customizable.
