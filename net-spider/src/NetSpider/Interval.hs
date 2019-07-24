2b3a6ab161c20980e050e95a9edc9750dd3063f5
Toshio Ito <toshio9.ito@toshiba.co.jp>
2019-07-25 03:51:47 +0900

    NetSpider.Interval: import functions from CLI.Timestamp.

--- net-spider/src/NetSpider/Interval.hs
+++ net-spider/src/NetSpider/Interval.hs
@@ -6,21 +6,88 @@
 -- 
 module NetSpider.Interval
   ( -- * Re-exports
-    -- ** Types
     Interval,
     Extended(..),
-    -- ** Functions
-    (<=..<=), (<..<=), (<=..<), (<..<),
+    interval, (<=..<=), (<..<=), (<=..<), (<..<),
+    -- * Types
+    IntervalEnd,
+    ErrorMsg,
+    -- * Parsers
+    parseTimeIntervalEnd,
+    parseIntervalEnd,
     -- * Utility
     secUpTo
   ) where
 
 import Data.ExtendedReal (Extended(..))
 import Data.Int (Int64)
-import Data.Interval (Interval, (<=..<=), (<..<=), (<=..<), (<..<))
-import qualified Data.Interval as Interval
+import Data.Interval (Interval, interval, (<=..<=), (<..<=), (<=..<), (<..<))
 
-import NetSpider.Timestamp (Timestamp, addSec)
+import NetSpider.Timestamp (Timestamp, addSec, parseTimestamp)
+
+-- | Upper or lower end of 'Interval'. The 'Bool' field is 'True' if
+-- the end is inclusive.
+type IntervalEnd a = (Extended a, Bool)
+
+-- | Error message type.
+type ErrorMsg = String
+
+-- | Parse the 'String' into 'IntervalEnd' @a@, with the
+-- user-supplied parser for @a@. See 'parseTimeIntervalEnd' for
+-- example.
+parseIntervalEnd :: (String -> Either ErrorMsg a) -- ^ parser for the type variable @a@
+                 -> String -- ^ input to be parsed
+                 -> Either ErrorMsg (IntervalEnd a)
+parseIntervalEnd parseFinite input = do
+  (is_inclusive, value_part) <- parseInclusive input
+  value <- parseValue value_part
+  return (value, is_inclusive)
+  where
+    parseInclusive "" = Right (True, "")
+    parseInclusive ('i' : rest) = Right (True, rest)
+    parseInclusive ('x' : rest) = Right (False, rest)
+    parseInclusive s = Right (True, s)
+    parseValue "+inf" = Right PosInf
+    parseValue "-inf" = Right NegInf
+    parseValue s = either (Left . makeErr) (Right . Finite) $ parseFinite s
+      where
+        makeErr e = "Parse error: " ++ s ++ ": " ++ e
+
+-- | Parse the 'String' into an end of time interval. It uses
+-- 'parseIntervalEnd'.
+--
+-- If the 'String' is prefixed with \'i\', the end is inclusive. If
+-- the prefix is \'x\', the end is exclusive. Without such prefix,
+-- the end is inclusive by default.
+--
+-- Timestamp is parsed by 'parseTimestamp'. Positive infinity is
+-- expressed as \'+inf\' (note that \'+\' is mandatory), and
+-- negative infinity as \'-inf\'.
+--
+-- >>> parseTimeIntervalEnd "2019-10-09T12:03:22"
+-- Right (Finite (Timestamp {epochTime = 1570622602000, timeZone = Nothing}),True)
+-- >>> parseTimeIntervalEnd "i2019-10-09T12:03:22"
+-- Right (Finite (Timestamp {epochTime = 1570622602000, timeZone = Nothing}),True)
+-- >>> parseTimeIntervalEnd "x2019-10-09T12:03:22"
+-- Right (Finite (Timestamp {epochTime = 1570622602000, timeZone = Nothing}),False)
+-- >>> parseTimeIntervalEnd "+inf"
+-- Right (PosInf,True)
+-- >>> parseTimeIntervalEnd "i+inf"
+-- Right (PosInf,True)
+-- >>> parseTimeIntervalEnd "x+inf"
+-- Right (PosInf,False)
+-- >>> parseTimeIntervalEnd "-inf"
+-- Right (NegInf,True)
+-- >>> parseTimeIntervalEnd "i-inf"
+-- Right (NegInf,True)
+-- >>> parseTimeIntervalEnd "x-inf"
+-- Right (NegInf,False)
+parseTimeIntervalEnd :: String -> Either ErrorMsg (IntervalEnd Timestamp)
+parseTimeIntervalEnd = parseIntervalEnd parseTimestampE
+  where
+    parseTimestampE t = maybe (Left err_msg) Right $ parseTimestamp t
+      where
+        err_msg = "Cannot parse as a Timestamp: " ++ t
 
 -- | @s `secUpTo` ts@ returns the time interval of length @s@ (in
 -- seconds) and up to @ts@. The interval is inclusive for both ends.
