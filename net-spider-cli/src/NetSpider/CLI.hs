5dd9b8fdc8278182e69c61d8eb7dcc1f06facbda
Toshio Ito <debug.ito@gmail.com>
2019-08-02 14:19:08 +0900

    cli: CLI module exports submodules.

--- net-spider-cli/src/NetSpider/CLI.hs
+++ net-spider-cli/src/NetSpider/CLI.hs
@@ -5,5 +5,9 @@
 -- Maintainer: Toshio Ito <debug.ito@gmail.com>
 -- 
 module NetSpider.CLI
-       ( 
+       ( module NetSpider.CLI.Spider,
+         module NetSpider.CLI.Snapshot
        ) where
+
+import NetSpider.CLI.Spider
+import NetSpider.CLI.Snapshot
