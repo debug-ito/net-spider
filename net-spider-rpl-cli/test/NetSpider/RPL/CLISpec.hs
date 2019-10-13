13ce0f7f154d4d75de82d81348e023e3a2c5d054
Toshio Ito <debug.ito@gmail.com>
2019-10-13 15:37:23 +0900

    rpl-cli: fix test.

--- net-spider-rpl-cli/test/NetSpider/RPL/CLISpec.hs
+++ net-spider-rpl-cli/test/NetSpider/RPL/CLISpec.hs
@@ -3,7 +3,8 @@ module NetSpider.RPL.CLISpec (main,spec) where
 import Data.Monoid (mempty)
 import qualified Options.Applicative as Opt
 import Net.IPv6 (ipv6)
-import NetSpider.Query (startsFrom)
+import NetSpider.CLI.Snapshot (makeSnapshotQuery)
+import NetSpider.Query (startsFrom, defQuery)
 import NetSpider.RPL.FindingID (IPv6ID(..))
 import Test.Hspec
 
@@ -27,8 +28,9 @@ spec = do
     specify "--starts-from" $ do
       let parse_result = runParser optionParser ["snapshot", "-s", "fd00::212:4b00:13a4:c554"]
       case parse_result of
-        Right (CLIConfig _ (CmdSnapshot got_q)) ->
-          startsFrom got_q `shouldBe` [IPv6ID $ ipv6 0xfd00 0 0 0 0x0212 0x4b00 0x13a4 0xc554]
+        Right (CLIConfig _ (CmdSnapshot got_cq)) -> do
+          let got_s = fmap startsFrom $ makeSnapshotQuery (defQuery []) got_cq
+          got_s `shouldBe` Right [IPv6ID $ ipv6 0xfd00 0 0 0 0x0212 0x4b00 0x13a4 0xc554]
         Right _ ->
           expectationFailure ("Unexpected CLIConfig obtained.")
         Left err ->
