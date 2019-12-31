e7cac1bc2d0d0ed3fb9fe842aeabda0d8db6bdb9
Toshio Ito <debug.ito@gmail.com>
2019-12-31 19:06:37 +0900

    TimestampSpec: add test cases.

--- net-spider/test/NetSpider/TimestampSpec.hs
+++ net-spider/test/NetSpider/TimestampSpec.hs
@@ -1,17 +1,23 @@
 module NetSpider.TimestampSpec (main,spec) where
 
+import Data.Aeson (encode, decode)
 import Test.Hspec
 
-import NetSpider.Timestamp (Timestamp)
+import NetSpider.Timestamp (Timestamp, fromS)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = describe "FromJSON and ToJSON" $ do
-  specTS undefined -- TODO
+  specTS $ fromS "2019-12-31T18:46"
+  specTS $ fromS "2019-12-31T18:46:10"
+  specTS $ fromS "2019-12-31 18:46:11.037"
+  specTS $ fromS "2019-09-21T00:32Z"
+  specTS $ fromS "2019-08-07 11:18:43+07:00"
+  specTS $ fromS "2019-08-07T11:18:43.112-02:30"
 
 
 specTS :: Timestamp -> Spec
 specTS t = specify (show t) $ do
-  True `shouldBe` False -- TODO
+  (decode $ encode t) `shouldBe` Just t
