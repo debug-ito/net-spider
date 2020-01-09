cc110b31ab517381536ec170d5d5c79fb8ee6aaa
Toshio Ito <debug.ito@gmail.com>
2020-01-09 22:24:00 +0900

    refactor test into JSONUtil

--- net-spider/test/NetSpider/FoundSpec.hs
+++ net-spider/test/NetSpider/FoundSpec.hs
@@ -2,9 +2,10 @@
 module NetSpider.FoundSpec (main,spec) where
 
 import qualified Data.Aeson as Aeson
-import Data.Either (isRight)
 import Data.Text (Text)
 import Test.Hspec
+
+import JSONUtil (specJSONFromTo)
 
 import NetSpider.Found (FoundLink(..), LinkState(..))
 
@@ -14,19 +15,11 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "FoundLink" $ do
-    specify "FromJSON and ToJSON" $ do
-      let input = "{\"target_node\":\"target\", \"link_state\": \"to_target\", \"link_attrs\":125}"
-          expected :: FoundLink Text Int
-          expected = FoundLink
-                     { targetNode = "target",
-                       linkState = LinkToTarget,
-                       linkAttributes = 125
-                     }
-          got_dec = Aeson.eitherDecode input
-          got_enc_v :: Either String Aeson.Value
-          got_enc_v = Aeson.eitherDecode $ Aeson.encode expected
-          exp_enc = Aeson.eitherDecode input
-      got_dec `shouldBe` Right expected
-      isRight exp_enc `shouldBe` True
-      got_enc_v `shouldBe` exp_enc
-      
+    specJSONFromTo
+      "Int attribute"
+      "{\"target_node\":\"target\", \"link_state\": \"to_target\", \"link_attrs\":125}"
+      FoundLink
+      { targetNode = ("target" :: Text),
+        linkState = LinkToTarget,
+        linkAttributes = (125 :: Int)
+      }
