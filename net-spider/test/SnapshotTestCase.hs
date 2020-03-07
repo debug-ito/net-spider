c4d8513c45ad0c94b8c44a55982f4e7c0f2221a0
Toshio Ito <debug.ito@gmail.com>
2020-03-07 17:44:45 +0900

    writing SnapshotTestCase.

--- net-spider/test/SnapshotTestCase.hs
+++ net-spider/test/SnapshotTestCase.hs
@@ -7,25 +7,31 @@ module SnapshotTestCase
 import Data.Aeson (ToJSON)
 import Data.Greskell (FromGraphSON)
 import Data.Hashable (Hashable)
-import Data.List (sort)
+import Data.List (sort, sortOn)
 import Data.Text (Text, pack)
 import qualified Data.Vector as V
-import Data.Vector ((!))
+import Data.Vector ((!), Vector)
 import Test.Hspec
 
 import NetSpider.Found (FoundNode(..), FoundLink(..), LinkState(..))
 import NetSpider.Graph (NodeAttributes, LinkAttributes)
-import NetSpider.Query (Query, defQuery)
+import NetSpider.Query (Query, defQuery, unifyLinkSamples)
 import NetSpider.Snapshot
   ( SnapshotLink, SnapshotGraph,
     nodeId, linkNodeTuple, isDirected, linkTimestamp,
     isOnBoundary, nodeTimestamp
   )
 import qualified NetSpider.Snapshot as S (nodeAttributes, linkAttributes)
+import NetSpider.Unify
+  ( UnifyStdConfig(..), defUnifyStdConfig, unifyStd, latestLinkSample
+  )
 
 import NetSpider.Timestamp (fromS)
 
-import TestCommon (AText(..), sortSnapshotElements)
+import TestCommon
+  ( AText(..), APorts(..),
+    sortSnapshotElements, subIdWithAPorts, alignAPortsToLinkDirection
+  )
 
 
 
@@ -55,6 +61,19 @@ oneNeighborFoundNodes = [nbs]
                       neighborLinks = return link,
                       nodeAttributes = ()
                     }
+
+queryWithAPorts :: Text -> Query Text () APorts APorts
+queryWithAPorts nid = (defQuery [nid]) { unifyLinkSamples = unifyStd unify_conf }
+  where
+    unify_conf = defUnifyStdConfig
+                 { makeLinkSubId = subIdWithAPorts,
+                   mergeSamples = \l r -> fmap alignAPortsToLinkDirection $ latestLinkSample (l ++ r)
+                 }
+
+sortLinksWithAttr :: (Ord n, Ord la) => Vector (SnapshotLink n la) -> Vector (SnapshotLink n la)
+sortLinksWithAttr = V.fromList . sortOn getKey . V.toList
+  where
+    getKey link = (linkNodeTuple link, S.linkAttributes link)
 
 snapshotTestCases :: [SnapshotTestCase]
 snapshotTestCases =
@@ -204,7 +223,243 @@ snapshotTestCases =
         in fns,
       caseQuery = defQuery ["n1"],
       caseAssert = multi_hop_neighbors_assert
+    },
+    SnapshotTestCase
+    { caseName = "loop network",
+      caseInput =
+        let fns :: [FoundNode Text () ()]
+            fns = [ FoundNode
+                    { subjectNode = "n1",
+                      foundAt = fromS "2018-12-01T10:00",
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkToTarget,
+                                          linkAttributes = ()
+                                        }
+                                      ],
+                      nodeAttributes = ()
+                    },
+                    FoundNode
+                    { subjectNode = "n2",
+                      foundAt = fromS "2018-12-01T15:00",
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n1",
+                                          linkState = LinkToSubject,
+                                          linkAttributes = ()
+                                        },
+                                        FoundLink
+                                        { targetNode = "n3",
+                                          linkState = LinkBidirectional,
+                                          linkAttributes = ()
+                                        }
+                                      ],
+                      nodeAttributes = ()
+                    },
+                    FoundNode
+                    { subjectNode = "n3",
+                      foundAt = fromS "2018-12-01T10:00",
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n1",
+                                          linkState = LinkToTarget,
+                                          linkAttributes = ()
+                                        },
+                                        FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkBidirectional,
+                                          linkAttributes = ()
+                                        }
+                                      ],
+                      nodeAttributes = ()
+                    }
+                  ]
+        in fns,
+      caseQuery = defQuery ["n1"],
+      caseAssert = loop_network_assert
+    },
+    SnapshotTestCase
+    { caseName = "multiple links between two nodes",
+      caseInput = 
+        let fns :: [FoundNode Text () APorts]
+            fns = [ FoundNode
+                    { subjectNode = "n1",
+                      foundAt = fromS "2018-12-01T20:00",
+                      nodeAttributes = (),
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkToTarget,
+                                          linkAttributes = APorts "p4" "p8"
+                                        },
+                                        FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkToTarget,
+                                          linkAttributes = APorts "p3" "p6"
+                                        },
+                                        FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkToTarget,
+                                          linkAttributes = APorts "p5" "p10"
+                                        }
+                                      ]
+                    },
+                    FoundNode
+                    { subjectNode = "n2",
+                      foundAt = fromS "2018-12-01T10:00",
+                      nodeAttributes = (),
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n1",
+                                          linkState = LinkToSubject,
+                                          linkAttributes = APorts "p6" "p3"
+                                        },
+                                        FoundLink
+                                        { targetNode = "n1",
+                                          linkState = LinkToSubject,
+                                          linkAttributes = APorts "p10" "p5"
+                                        },
+                                        FoundLink
+                                        { targetNode = "n1",
+                                          linkState = LinkToSubject,
+                                          linkAttributes = APorts "p8" "p4"
+                                        }
+                                      ]
+                    }
+                  ]
+        in fns,
+      caseQuery = queryWithAPorts "n1",
+      caseAssert = multiple_links_assert
+    },
+    SnapshotTestCase
+    { caseName = "link disappears",
+      caseInput = 
+        let fns :: [FoundNode Text () ()]
+            fns = [ FoundNode
+                    { subjectNode = "n1",
+                      foundAt = fromS "2018-12-01T10:00",
+                      nodeAttributes = (),
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkBidirectional,
+                                          linkAttributes = ()
+                                        }
+                                      ]
+                    },
+                    FoundNode
+                    { subjectNode = "n2",
+                      foundAt = fromS "2018-12-01T20:00",
+                      nodeAttributes = (),
+                      neighborLinks = []
+                    }
+                  ]
+        in fns,
+      caseQuery = defQuery ["n1"],
+      caseAssert = \got_graph -> do
+        let (got_ns, got_ls) = sortSnapshotElements got_graph
+        nodeId (got_ns ! 0) `shouldBe` "n1"
+        nodeId (got_ns ! 1) `shouldBe` "n2"
+        V.length got_ns `shouldBe` 2
+        V.length got_ls `shouldBe` 0
+        -- the n2 observes at t=200 that there is no link to n1. So the
+        -- Spider should consider the link disappears.
+    },
+    SnapshotTestCase
+    { caseName = "link appears",
+      caseInput = 
+        let fns :: [FoundNode Text () ()]
+            fns = [ FoundNode
+                    { subjectNode = "n1",
+                      foundAt = fromS "2018-12-01T20:00",
+                      nodeAttributes = (),
+                      neighborLinks = [ FoundLink
+                                        { targetNode = "n2",
+                                          linkState = LinkBidirectional,
+                                          linkAttributes = ()
+                                        }
+                                      ]
+                    },
+                    FoundNode
+                    { subjectNode = "n2",
+                      foundAt = fromS "2018-12-01T10:00",
+                      nodeAttributes = (),
+                      neighborLinks = []
+                    }
+                  ]
+        in fns,
+      caseQuery = defQuery ["n1"],
+      caseAssert = \got_graph -> do
+        let (got_ns, got_ls) = sortSnapshotElements got_graph
+        nodeId (got_ns ! 0) `shouldBe` "n1"
+        nodeId (got_ns ! 1) `shouldBe` "n2"
+        V.length got_ns `shouldBe` 2
+        linkNodeTuple (got_ls ! 0) `shouldBe` ("n1", "n2")
+        isDirected (got_ls ! 0) `shouldBe` False
+        linkTimestamp (got_ls ! 0) `shouldBe` fromS "2018-12-01T20:00"
+        V.length got_ls `shouldBe` 1
+        -- the n2 observes at t=100 that there is no link to n1, but n1
+        -- observes there is a link at t=200.  Spider should consider the
+        -- link appears.
+    },
+    SnapshotTestCase
+    { caseName = "multiple links between pair, some appear, some disappear.",
+      caseInput = 
+        let fns :: [FoundNode Text () APorts]
+            fns = [ FoundNode
+                    { subjectNode = "n2",
+                      foundAt = fromS "2018-12-01T20:00",
+                      nodeAttributes = (),
+                      neighborLinks = links2
+                    },
+                    FoundNode
+                    { subjectNode = "n1",
+                      foundAt = fromS "2018-12-01T10:00",
+                      nodeAttributes = (),
+                      neighborLinks = links1
+                    }
+                  ]
+            links1 = [ FoundLink -- disappears
+                       { targetNode = "n2",
+                         linkState = LinkToTarget,
+                         linkAttributes = APorts "p11" "p21"
+                       },
+                       FoundLink -- stays
+                       { targetNode = "n2",
+                         linkState = LinkToTarget,
+                         linkAttributes = APorts "p12" "p22"
+                       }
+                     ]
+            links2 = [ FoundLink -- appears
+                       { targetNode = "n1",
+                         linkState = LinkToSubject,
+                         linkAttributes = APorts "p23" "p13"
+                       },
+                       FoundLink -- stays
+                       { targetNode = "n1",
+                         linkState = LinkToSubject,
+                         linkAttributes = APorts "p22" "p12" 
+                       }
+                     ]
+        in fns,
+      caseQuery = queryWithAPorts "n1",
+      caseAssert = \got_graph -> do
+        let (got_ns, got_ls_raw) = sortSnapshotElements got_graph
+        nodeId (got_ns ! 0) `shouldBe` "n1"
+        nodeTimestamp (got_ns ! 0) `shouldBe` (Just $ fromS "2018-12-01T10:00")
+        nodeId (got_ns ! 1) `shouldBe` "n2"
+        nodeTimestamp (got_ns ! 1) `shouldBe` (Just $ fromS "2018-12-01T20:00")
+        V.length got_ns `shouldBe` 2
+        let got_ls = sortLinksWithAttr got_ls_raw
+        linkNodeTuple (got_ls ! 0) `shouldBe` ("n1", "n2")
+        S.linkAttributes (got_ls ! 0) `shouldBe` APorts "p12" "p22"
+        linkTimestamp (got_ls ! 0) `shouldBe` fromS "2018-12-01T20:00"
+        linkNodeTuple (got_ls ! 1) `shouldBe` ("n1", "n2") -- TODO: looks like this link is removed. why?
+        S.linkAttributes (got_ls ! 1) `shouldBe` APorts "p13" "p23"
+        linkTimestamp (got_ls ! 1) `shouldBe` fromS "2018-12-01T20:00"
+        V.length got_ls `shouldBe` 2
     }
+
+-- TODO: how linkState relates to the property of SnapshotLink ?
+-- especially Bidirectional links? -> important thing is that if the
+-- link is Bidirectional, (source, destination) of snapshot link is
+-- always (subject, target).
+
   ]
   where
     one_neighbor_assert (got_ns, got_ls) = do
@@ -309,8 +564,43 @@ snapshotTestCases =
       linkTimestamp (got_ls ! 3) `shouldBe` fromS "2018-12-01T20:00"
       S.linkAttributes (got_ls ! 3) `shouldBe` AText "n4 to next"
       V.length got_ls `shouldBe` 4
-
-      
+    loop_network_assert got_graph = do
+      let (got_ns, got_ls) = sortSnapshotElements got_graph
+      nodeId (got_ns ! 0) `shouldBe` "n1"
+      isOnBoundary (got_ns ! 0) `shouldBe` False
+      nodeTimestamp (got_ns ! 0) `shouldBe` Just (fromS "2018-12-01T10:00")
+      nodeId (got_ns ! 1) `shouldBe` "n2"
+      isOnBoundary (got_ns ! 1) `shouldBe` False
+      nodeTimestamp (got_ns ! 1) `shouldBe` Just (fromS "2018-12-01T15:00")
+      nodeId (got_ns ! 2) `shouldBe` "n3"
+      isOnBoundary (got_ns ! 2) `shouldBe` False
+      nodeTimestamp (got_ns ! 2) `shouldBe` Just (fromS "2018-12-01T10:00")
+      V.length got_ns `shouldBe` 3
+      linkNodeTuple (got_ls ! 0) `shouldBe` ("n1", "n2")
+      isDirected (got_ls ! 0) `shouldBe` True
+      linkTimestamp (got_ls ! 0) `shouldBe` fromS "2018-12-01T15:00"
+      linkNodeTuple (got_ls ! 1) `shouldBe` ("n2", "n3")
+      isDirected (got_ls ! 1) `shouldBe` False
+      linkTimestamp (got_ls ! 1) `shouldBe` fromS "2018-12-01T15:00"
+      linkNodeTuple (got_ls ! 2) `shouldBe` ("n3", "n1")
+      isDirected (got_ls ! 2) `shouldBe` True
+      linkTimestamp (got_ls ! 2) `shouldBe` fromS "2018-12-01T10:00"
+    multiple_links_assert got_graph = do
+      let (got_ns, got_ls_raw) = sortSnapshotElements got_graph
+      nodeId (got_ns ! 0) `shouldBe` "n1"
+      nodeId (got_ns ! 1) `shouldBe` "n2"
+      V.length got_ns `shouldBe` 2
+      let got_ls = sortLinksWithAttr got_ls_raw
+      linkNodeTuple (got_ls ! 0) `shouldBe` ("n1", "n2")
+      S.linkAttributes (got_ls ! 0) `shouldBe` APorts "p3" "p6"
+      linkTimestamp (got_ls ! 0) `shouldBe` fromS "2018-12-01T20:00"
+      linkNodeTuple (got_ls ! 1) `shouldBe` ("n1", "n2")
+      S.linkAttributes (got_ls ! 1) `shouldBe` APorts "p4" "p8"
+      linkTimestamp (got_ls ! 1) `shouldBe` fromS "2018-12-01T20:00"
+      linkNodeTuple (got_ls ! 2) `shouldBe` ("n1", "n2")
+      S.linkAttributes (got_ls ! 2) `shouldBe` APorts "p5" "p10"
+      linkTimestamp (got_ls ! 2) `shouldBe` fromS "2018-12-01T20:00"
+      V.length got_ls `shouldBe` 3
 
       
 
