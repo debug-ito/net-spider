94f837f3e211e7f329fb273dfea8be7a55fd92cb
Toshio Ito <debug.ito@gmail.com>
2020-03-29 15:38:09 +0900

    write skeleton of SeqID.

--- net-spider/src/NetSpider/SeqID.hs
+++ net-spider/src/NetSpider/SeqID.hs
@@ -5,10 +5,45 @@
 --
 -- 
 module NetSpider.SeqID
-  ( SeqIDMaker
+  ( -- * Type
+    SeqIDMaker,
+    -- * Construction
+    newSeqIDMaker,
+    convertGraph,
+    convertLink,
+    convertNode,
+    convertNodeID
   ) where
+
+import NetSpider.Snapshot (SnapshotNode, SnapshotLink, SnapshotGraph)
 
 -- | 'SeqIDMaker' converts node ID type @n@ into the new node ID type
 -- @i@. The type @i@ is supposed to be an 'Enum', and it generates the
 -- node ID of type @i@ sequentially for each node ID of type @n@.
-data SeqIDMaker n i
+data SeqIDMaker n i = SeqIDMaker
+                    deriving (Show,Eq)
+
+-- | Make a 'SeqIDMaker' with the given initial ID.
+newSeqIDMaker :: i -- ^ Initial ID
+              -> SeqIDMaker n i
+newSeqIDMaker = undefined
+
+-- | Convert the given node ID of type @n@ into the sequential ID of
+-- type @i@.
+convertNodeID :: SeqIDMaker n i
+              -> n -- ^ Old ID
+              -> (SeqIDMaker n i, i) -- ^ Updated 'SeqIDMaker' and the new ID.
+convertNodeID = undefined
+
+-- | Convert node IDs in the 'SnapshotGraph'.
+convertGraph :: SeqIDMaker n i -> SnapshotGraph n na la -> (SeqIDMaker n i, SnapshotGraph i na la)
+convertGraph = undefined
+
+-- | Convert node IDs in the 'SnapshotLink'.
+convertLink :: SeqIDMaker n i -> SnapshotLink n la -> (SeqIDMaker n i, SnapshotLink i la)
+convertLink = undefined
+
+-- | Convert node ID in the 'SnapshotNode'.
+convertNode :: SeqIDMaker n i -> SnapshotNode n na -> (SeqIDMaker n i, SnapshotNode i na)
+convertNode = undefined
+
---
+++ net-spider/test/NetSpider/SeqIDSpec.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE OverloadedStrings #-}
+module NetSpider.SeqIDSpec (main,spec) where
+
+import Data.Text (Text)
+import Test.Hspec
+
+import NetSpider.SeqID
+  ( newSeqIDMaker, convertNodeID, convertGraph
+  )
+import NetSpider.Snapshot.Internal (SnapshotNode(..), SnapshotLink(..))
+import NetSpider.Snapshot (nodeId, linkNodeTuple)
+import NetSpider.Timestamp (fromEpochMillisecond)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = describe "SeqIDMaker" $ do
+  specify "first ID" $ do
+    let m = newSeqIDMaker (0 :: Int)
+        (got_m, got) = convertNodeID m ("hoge" :: Text)
+    got `shouldBe` 0
+    (snd $ convertNodeID got_m "hoge") `shouldBe` 0
+  specify "second ID" $ do
+    let m = newSeqIDMaker (0 :: Int)
+        (m1, _) = convertNodeID m ("bar" :: Text)
+        (got_m, got) = convertNodeID m1 ("foo" :: Text)
+    got `shouldBe` 1
+    (snd $ convertNodeID got_m "bar") `shouldBe` 0
+    (snd $ convertNodeID got_m "foo") `shouldBe` 1
+  specify "duplicate ID" $ do
+    let m = newSeqIDMaker (5 :: Int)
+        (m1, id1) = convertNodeID m ("bar" :: Text)
+        (_,  id2) = convertNodeID m1 ("bar" :: Text)
+    id1 `shouldBe` 5
+    id2 `shouldBe` 5
+  specify "convertGraph" $ do
+    let input = (nodes, links)
+        mkNode :: Text -> SnapshotNode Text ()
+        mkNode nid = SnapshotNode
+                     { _nodeId = nid,
+                       _isOnBoundary = False,
+                       _nodeTimestamp = Just $ fromEpochMillisecond 100,
+                       _nodeAttributes = Just ()
+                     }
+        mkLink :: Text -> Text -> SnapshotLink Text ()
+        mkLink source dest = SnapshotLink
+                             { _sourceNode = source,
+                               _destinationNode = dest,
+                               _isDirected = True,
+                               _linkTimestamp = fromEpochMillisecond 100,
+                               _linkAttributes = ()
+                             }
+        nodes = [ mkNode "n1",
+                  mkNode "n2",
+                  mkNode "n3",
+                  mkNode "n4",
+                  mkNode "n5"
+                ]
+        links = [ mkLink "n1" "n3",
+                  mkLink "n4" "n2",
+                  mkLink "n3" "n5",
+                  mkLink "n5" "n2"
+                ]
+        (_, (got_nodes, got_links)) = convertGraph (newSeqIDMaker (1 :: Int)) input
+    map nodeId got_nodes `shouldBe` [1,2,3,4,5]
+    map linkNodeTuple got_links `shouldBe`
+      [ (1, 3), (4, 2), (3, 5), (5, 2)
+      ]
+  
