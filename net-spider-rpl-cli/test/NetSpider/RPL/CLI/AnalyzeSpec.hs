b40af064c1bbc732ad3bc5b3a3ad978d9902420b
Toshio Ito <debug.ito@gmail.com>
2020-04-10 18:16:51 +0900

    rpl-cli: AnalyzeSpec: test error logs.

--- net-spider-rpl-cli/test/NetSpider/RPL/CLI/AnalyzeSpec.hs
+++ net-spider-rpl-cli/test/NetSpider/RPL/CLI/AnalyzeSpec.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module NetSpider.RPL.CLI.AnalyzeSpec (main,spec) where
 
+import Control.Monad.Logger (LogLevel(..))
+import System.Log.FastLogger (fromLogStr)
+import qualified Data.ByteString as BS
 import Data.Foldable (foldl')
 import Data.Hashable (Hashable)
 import Data.Int (Int64)
 import Data.Maybe (fromJust)
-import Data.Text (Text)
+import Data.Text (Text, unpack)
+import Data.Text.Encoding (encodeUtf8)
 import NetSpider.Found (FoundNode(..), FoundLink(..), LinkState(..))
-import NetSpider.Log (runWriterLoggingM)
+import NetSpider.Log (runWriterLoggingM, LogLine)
 import NetSpider.Query (foundNodePolicy, unifyLinkSamples, Query)
 import NetSpider.RPL.FindingID (FindingID, idFromText, ipv6FromText, IPv6ID)
 import NetSpider.RPL.DAO
@@ -99,12 +103,32 @@ dioNode ts sub targets =
       }
     defNRank = 512
 
+expectNoErrorLog :: [LogLine] -> IO ()
+expectNoErrorLog got_logs =
+  if (length $ filter matchLog got_logs) == 0
+  then return ()
+  else expectationFailure ("Expected no error/warn log, but found. " <> show got_logs)
+  where
+    matchLog (_, _, LevelError, _) = True
+    matchLog (_, _, LevelWarn, _) = True
+    matchLog (_, _, _, _) = False
+
+expectErrorLog :: [LogLine] -> Text -> IO ()
+expectErrorLog got_logs exp_err_msg =
+  if (length $ filter matchLog got_logs) == 0
+  then expectationFailure ("Expected an error log matching " <> unpack exp_err_msg <> ", but not found.")
+  else return ()
+  where
+    matchLog (_, _, LevelError, logstr) = matchLogMsg logstr
+    matchLog (_, _, _, _) = False
+    matchLogMsg logstr = encodeUtf8 exp_err_msg `BS.isInfixOf` fromLogStr logstr
+
 spec_DIO :: Spec
 spec_DIO = describe "analyzeDIO" $ do
   specify "root only" $ do
     let fns = [ dioNode 100 "dio://[fd00::1]" []
               ]
-        (got, _) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
+        (got, logs) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 1,
@@ -114,13 +138,14 @@ spec_DIO = describe "analyzeDIO" $ do
           A.time = fromEpochMillisecond 100
         }
       )
+    expectNoErrorLog logs
   specify "depth 1" $ do
     let fns = [ dioNode 100 "dio://[fd00::1]" [],
                 dioNode 120 "dio://[fd00::2]" ["dio://[fd00::1]"],
                 dioNode 140 "dio://[fd00::3]" ["dio://[fd00::1]"],
                 dioNode 110 "dio://[fd00::4]" ["dio://[fd00::1]"]
               ]
-        (got, _) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
+        (got, logs) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 4,
@@ -130,6 +155,7 @@ spec_DIO = describe "analyzeDIO" $ do
           A.time = fromEpochMillisecond 140
         }
       )
+    expectNoErrorLog logs
   specify "depth 4" $ do
     let fns = [ dioNode 150 "dio://[fd00::1]" [],
                 dioNode 110 "dio://[fd00::4]" ["dio://[fd00::3]"],
@@ -138,7 +164,7 @@ spec_DIO = describe "analyzeDIO" $ do
                 dioNode 170 "dio://[fd00::5]" ["dio://[fd00::4]"],
                 dioNode 189 "dio://[fd00::6]" ["dio://[fd00::2]"]
               ]
-        (got, _) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
+        (got, logs) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 6,
@@ -148,13 +174,14 @@ spec_DIO = describe "analyzeDIO" $ do
           A.time = fromEpochMillisecond 200
         }
       )
+    expectNoErrorLog logs
   specify "multiple orphan nodes with one root" $ do
     let fns = [ dioNode 110 "dio://[fd00::1]" [],
                 dioNode 120 "dio://[fd00::2]" [],
                 dioNode 130 "dio://[fd00::3]" [],
                 dioNode 100 "dio://[fd00::4]" ["dio://[fd00::2]"]
               ]
-        (got, _) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
+        (got, logs) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 4,
@@ -164,14 +191,16 @@ spec_DIO = describe "analyzeDIO" $ do
           A.time = fromEpochMillisecond 130
         }
       )
+    expectNoErrorLog logs
   specify "multiple roots" $ do
     let fns = [ dioNode 100 "dio://[fd00::1]" [],
                 dioNode 200 "dio://[fd00::2]" ["dio://[fd00::1]"],
                 dioNode 150 "dio://[fd00::3]" [],
                 dioNode 130 "dio://[fd00::4]" ["dio://[fd00::3]"]
               ]
-        (got, _) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
+        (got, logs) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
     got `shouldBe` Nothing
+    expectErrorLog logs "multiple root"
   specify "all orphan nodes" $ do
     let fns = [ dioNode 120 "dio://[fd00::1]" [],
                 dioNode 170 "dio://[fd00::2]" [],
@@ -179,8 +208,9 @@ spec_DIO = describe "analyzeDIO" $ do
                 dioNode 100 "dio://[fd00::3]" [],
                 dioNode 180 "dio://[fd00::4]" []
               ]
-        (got, _) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
+        (got, logs) = runWriterLoggingM $ analyzeDIO $ makeSnapshotDIO fns
     got `shouldBe` Nothing
+    expectErrorLog logs "orphan nodes only"
 
 
 spec_DAO :: Spec
@@ -188,7 +218,7 @@ spec_DAO = describe "analyzeDAO" $ do
   specify "root only" $ do
     let fns = [ daoNode 100 (Just 0) "dao://[fd00::1]" []
               ]
-        (got, _) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
+        (got, logs) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 1,
@@ -198,6 +228,7 @@ spec_DAO = describe "analyzeDAO" $ do
           A.time = fromEpochMillisecond 100
         }
       )
+    expectNoErrorLog logs
   specify "depth 1" $ do
     let fns = [ daoNode 100 (Just 3) "dao://[fd00::1]"
                 [ "dao://[fd00::2]",
@@ -208,7 +239,7 @@ spec_DAO = describe "analyzeDAO" $ do
                 daoNode 100 Nothing "dao://[fd00::3]" [],
                 daoNode 100 Nothing "dao://[fd00::4]" []
               ]
-        (got, _) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
+        (got, logs) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 4,
@@ -218,6 +249,7 @@ spec_DAO = describe "analyzeDAO" $ do
           A.time = fromEpochMillisecond 100
         }
       )
+    expectNoErrorLog logs
   specify "depth 4" $ do
     let fns = [ daoNode 100 Nothing  "dao://[fd00::3]" ["dao://[fd00::4]"],
                 daoNode 100 (Just 4) "dao://[fd00::1]" ["dao://[fd00::2]"],
@@ -225,7 +257,7 @@ spec_DAO = describe "analyzeDAO" $ do
                 daoNode 100 Nothing  "dao://[fd00::2]" ["dao://[fd00::3]"],
                 daoNode 100 Nothing  "dao://[fd00::4]" ["dao://[fd00::5]"]
               ]
-        (got, _) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
+        (got, logs) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 5,
@@ -235,6 +267,7 @@ spec_DAO = describe "analyzeDAO" $ do
           A.time = fromEpochMillisecond 100
         }
       )
+    expectNoErrorLog logs
   specify "multiple timestamps" $ do
     let fns = [ daoNode 100 (Just 4) "dao://[fd00::1]" ["dao://[fd00::2]", "dao://[fd00::3]"],
                 daoNode 100 Nothing  "dao://[fd00::2]" ["dao://[fd00::4]", "dao://[fd00::5]"],
@@ -250,7 +283,7 @@ spec_DAO = describe "analyzeDAO" $ do
                 -- daoNode 200 Nothing  "dao://[fd00::4]" [],
                 -- daoNode 200 Nothing  "dao://[fd00::5]" [],
               ]
-        (got, _) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
+        (got, logs) = runWriterLoggingM $ analyzeDAO $ makeSnapshotDAO fns
     got `shouldBe`
       ( Just $ DODAGAttributes
         { A.node_num = 6,
@@ -260,6 +293,7 @@ spec_DAO = describe "analyzeDAO" $ do
           A.time = fromEpochMillisecond 200
         }
       )
+    expectNoErrorLog logs
 
       
   
